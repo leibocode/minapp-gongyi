808289e5ac2fc90e5c913d2ec393cceb76f325e2
Source <Smileioc@163.com>
2018-11-29 00:52:56 +0800

    美化&完善组织详情

--- client/pages/organi_details/organi_details.js
+++ client/pages/organi_details/organi_details.js
@@ -1,5 +1,36 @@
 Page({
-    data:{
-        
+    data: {
+        id: 1,
+        delinfo: {
+            photo: '../../images/organi-pic2.jpg',
+            organisStatus: 1,
+            type: '其他',
+            city: '广东广州市越秀区',
+            content: "测试描述测试，描述测试描述测试描述测试，描述测试描述"
+        },
+        dzinfo: {
+            id: 1,
+            photo: '../../images/photo-pic.jpg',
+            name: 'Meizi',
+            iphonexs: '182****9290',
+            organisStatus: 1
+        },
+        p_list: [{
+            id: 1,
+            photo: '../../images/photo-pic.jpg',
+            name: 'Leibo',
+            iphonexs: '182****9290',
+            organisStatus: 1
+        }, {
+            id: 2,
+            photo: '../../images/photo-pic.jpg',
+            name: 'Leibo V666',
+            iphonexs: '182****9290',
+            organisStatus: 1
+          }]
+    },
+    onload: function () {
+
+        console.log(this.data.delinfo);
     }
 })
